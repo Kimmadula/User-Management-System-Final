71d54a3ed010c2b576f798985cb641493b39d3c1
Louie Jee I. Gijan <louiejee2422@gmail.com>
2025-05-18 18:14:54 +0800

    Fix workflows route and ensure file is tracked

--- backend/workflows/index.js
+++ backend/workflows/index.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const workflowController = require('./workflow.controller');
 
 router.get('/', (req, res) => {
   res.json({ message: "Workflows endpoint working!" });
 });
-
-module.exports = router;
-
-// Forward all workflow routes to the workflow controller
-router.use('/', workflowController);
 
 module.exports = router;
