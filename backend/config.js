141351f13c5d0370f61804060f903c8a1d540e6a
Iris Tumakay <iristumakay2001@gmail.com>
2025-05-18 06:10:28 -0400

    pls na

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,13 @@
 module.exports = {
   database: {
     host: process.env.DB_HOST || "gondola.proxy.rlwy.net",
-    port: process.env.DB_PORT || 	58293,
+    port: process.env.DB_PORT || 58293,
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "qnrzhqBAsdQmMkQBOjZBuOXGBQWIJVJm",
     database: process.env.DB_NAME || "railway"
+  },
+  connectionString: {
+    mysql: process.env.MYSQL_URL || "mysql://root:qnrzhqBAsdQmMkQBOjZBuOXGBQWIJVJm@gondola.proxy.rlwy.net:58293/railway"
   },
   secret: process.env.JWT_SECRET || "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET",
   emailFrom: "info@node-mysql-signup-verification-api.com",
