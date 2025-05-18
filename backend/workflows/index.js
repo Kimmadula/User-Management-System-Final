6073aafb5b6697af32b14793b960c9fbb3324f91
Louie Jee I. Gijan <louiejee2422@gmail.com>
2025-05-18 18:26:37 +0800

    Remove duplicate workflows folder from root

--- backend/workflows/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: "Workflows endpoint working!" });
-});
-
-module.exports = router;
