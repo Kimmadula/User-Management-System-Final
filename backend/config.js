736c04582c0df5c1951e695627578cd109120c7b
Iris Tumakay <iristumakay2001@gmail.com>
2025-05-18 07:18:29 -0400

    removed the gmail and added the ethereal back

--- backend/config.js
+++ backend/config.js
@@ -10,15 +10,13 @@ module.exports = {
     mysql: process.env.MYSQL_URL || "mysql://root:qnrzhqBAsdQmMkQBOjZBuOXGBQWIJVJm@gondola.proxy.rlwy.net:58293/railway"
   },
   secret: process.env.JWT_SECRET || "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET",
-  emailFrom: "iristumakaydummy@gmail.com",
+  emailFrom: "info@node-mysql-signup-verification-api.com",
   smtpOptions: {
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    host: "smtp.ethereal.email",
+    port: 587,
     auth: {
-      user: "iristumakaydummy@gmail.com",
-      pass: "4p2u d67m k66p 3osf 7ijq l4ga ebhj rwwt"
+      user: "colin.morar@ethereal.email",
+      pass: "AAPwTyBe5B8xZd3f6e"
     }
   }
 };
-
