7ff25afdd175c41864828887914200b9aa3246f1
Louie Jee I. Gijan <louiejee2422@gmail.com>
2025-05-20 17:38:49 +0800

    Added user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "User",
+    },
+  })
+  return User
+}
