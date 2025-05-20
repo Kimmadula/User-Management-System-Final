21a4592bdc865a4f1048aedbe3f9de3e884004ea
Louie Jee I. Gijan <louiejee2422@gmail.com>
2025-05-20 22:37:25 +0800

    Update alert

--- frontend/src/app/_components/alert.component.ts
+++ frontend/src/app/_components/alert.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, OnDestroy } from "@angular/core"
 import { Router, NavigationStart } from "@angular/router"
 import { Subscription } from "rxjs"
-
 import { Alert, AlertType } from "@app/_models"
 import { AlertService } from "@app/_services"
 
